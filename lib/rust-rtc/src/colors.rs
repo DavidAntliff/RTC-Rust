a87189f7d6972f32b936d1a4783379e7093a6aaf
David Antliff <d.antliff@unsw.edu.au>
2023-04-12 19:13:46 +1200

    Refactor Color to be a newtype of Tuple, rather than a type alias to Tuple.

--- lib/rust-rtc/src/colors.rs
+++ lib/rust-rtc/src/colors.rs
@@ -1,9 +1,37 @@
 use super::tuples::{Tuple};
+use derive_more::{Add, Sub, Neg, Mul};
 
-type Color = Tuple;
+#[derive(Debug, Default, PartialEq, Add, Sub, Neg, Mul)]
+pub struct Color(Tuple);
+
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Color {
+        Color(Tuple::new(r, g, b, 0.0))
+    }
+
+    pub fn red(&self) -> f64 { self.0.x() }
+    pub fn green(&self) -> f64 { self.0.y() }
+    pub fn blue(&self) -> f64 { self.0.z() }
+}
+
+macro_rules! color_mul {
+    ( $lhs:ty , $rhs:ty ) => {
+        impl std::ops::Mul<$rhs> for $lhs {
+            type Output = Color;
+            fn mul(self, rhs: $rhs) -> Color {
+                Color(&self.0 * &rhs.0)
+            }
+        }
+    }
+}
+
+color_mul!(Color, Color);
+color_mul!(Color, &Color);
+color_mul!(&Color, Color);
+color_mul!(&Color, &Color);
 
 pub fn color(r: f64, g: f64, b: f64) -> Color {
-    Tuple::new(r, g, b, 0.0)
+    Color::new(r, g, b)
 }
 
 /// Hadamard or Shur Product
@@ -11,17 +39,36 @@ pub fn hadamard(lhs: &Color, rhs: &Color) -> Color {
     lhs * rhs
 }
 
-impl Color {
-    pub fn red(&self) -> f64 { self.x() }
-    pub fn green(&self) -> f64 { self.y() }
-    pub fn blue(&self) -> f64 { self.z() }
-}
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use approx::assert_relative_eq;
+    use approx::{assert_relative_eq, AbsDiffEq, RelativeEq};
+
+    impl AbsDiffEq for Color {
+        type Epsilon = f64;
+
+        fn default_epsilon() -> f64 {
+            f64::default_epsilon()
+        }
+
+        fn abs_diff_eq(&self, other: &Self, epsilon: f64) -> bool {
+            self.0.0.abs_diff_eq(other.0.0, epsilon)
+        }
+    }
+
+    impl RelativeEq for Color {
+        fn default_max_relative() -> f64 {
+            f64::default_max_relative()
+        }
+
+        fn relative_eq(&self, other: &Self, epsilon: f64, max_relative: f64) -> bool {
+            f64::relative_eq(&self.0.0.x, &other.0.0.x, epsilon, max_relative) &&
+                f64::relative_eq(&self.0.0.y, &other.0.0.y, epsilon, max_relative) &&
+                f64::relative_eq(&self.0.0.z, &other.0.0.z, epsilon, max_relative) &&
+                f64::relative_eq(&self.0.0.w, &other.0.0.w, epsilon, max_relative)
+        }
+    }
 
     // Colors are (red, green, blue) tuples
     #[test]
